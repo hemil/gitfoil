1f8de6ae7836de578563f6d1e46e1dc74e66b0fa
hemil <hemil.shah@grofers.com>
2017-08-07 22:47:09 +0530

    redirect index to admin

--- gitfoil/urls.py
+++ gitfoil/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include('wrapper.urls')),
+    url(r'^.*$', RedirectView.as_view(url='admin/', permanent=False), name='main_index'),
 ]
